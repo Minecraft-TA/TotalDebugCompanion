180a4c344537d3389a452a14e67c5df16d42b62d
Pelotrio <45769595+Pelotrio@users.noreply.github.com>
2022-01-01 21:46:50 +0100

    Disable and clear packets on tab close

--- src/main/java/com/github/minecraft_ta/totalDebugCompanion/model/PacketLoggerView.java
+++ src/main/java/com/github/minecraft_ta/totalDebugCompanion/model/PacketLoggerView.java
@@ -1,5 +1,8 @@
 package com.github.minecraft_ta.totalDebugCompanion.model;
 
+import com.github.minecraft_ta.totalDebugCompanion.CompanionApp;
+import com.github.minecraft_ta.totalDebugCompanion.messages.packetLogger.PacketClearMessage;
+import com.github.minecraft_ta.totalDebugCompanion.messages.packetLogger.PacketLoggerStateChangeMessage;
 import com.github.minecraft_ta.totalDebugCompanion.ui.components.editors.PacketLoggerViewPanel;
 
 import javax.swing.*;
@@ -25,5 +28,12 @@ public class PacketLoggerView implements IEditorPanel {
     @Override
     public Component getComponent() {
         return new PacketLoggerViewPanel();
+    }
+
+    @Override
+    public boolean canClose() {
+        CompanionApp.SERVER.getMessageProcessor().enqueueMessage(new PacketLoggerStateChangeMessage(false, false));
+        CompanionApp.SERVER.getMessageProcessor().enqueueMessage(new PacketClearMessage());
+        return true;
     }
 }

76fc911ecea0fe6121c04224c202ed26e0f9c0c3
tth05 <36999320+tth05@users.noreply.github.com>
2022-04-05 20:12:33 +0200

    Fix curly bracket auto-closing behaving incorrectly

--- src/main/java/com/github/minecraft_ta/totalDebugCompanion/jdt/semanticHighlighting/CustomJavaTokenMaker.java
+++ src/main/java/com/github/minecraft_ta/totalDebugCompanion/jdt/semanticHighlighting/CustomJavaTokenMaker.java
@@ -1,12 +1,17 @@
 package com.github.minecraft_ta.totalDebugCompanion.jdt.semanticHighlighting;
 
 import com.github.minecraft_ta.totalDebugCompanion.jdt.diagnostics.ASTCache;
+import org.fife.ui.rsyntaxtextarea.RSyntaxDocument;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rsyntaxtextarea.Token;
 import org.fife.ui.rsyntaxtextarea.TokenTypes;
 import org.fife.ui.rsyntaxtextarea.modes.JavaTokenMaker;
 
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
 import javax.swing.text.Segment;
+import javax.swing.text.TextAction;
+import java.awt.event.ActionEvent;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -43,5 +48,65 @@ public class CustomJavaTokenMaker extends JavaTokenMaker {
         }
 
         return firstToken;
+    }
+
+    @Override
+    public Action getInsertBreakAction() {
+        return new TextAction("customInsertBreak") {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                var area = (RSyntaxTextArea) getTextComponent(e);
+                var document = (RSyntaxDocument) area.getDocument();
+
+                var caretPos = area.getCaretPosition();
+                var root = document.getDefaultRootElement();
+                var line = root.getElement(root.getElementIndex(caretPos));
+                var start = line.getStartOffset();
+                var len = line.getEndOffset() - 1 - start;
+                try {
+                    var lineText = document.getText(start, len);
+                    var tabCount = getTabCount(lineText);
+                    var builder = new StringBuilder("\n");
+                    builder.append("\t".repeat(tabCount + (document.getText(caretPos - 1, 1).equals("{") ? 1 : 0)));
+
+                    caretPos += builder.length();
+                    if (getOpenBraceCount(document) > 0)
+                        builder.append("\n").append("\t".repeat(tabCount)).append("}");
+
+                    area.insert(builder.toString(), area.getCaretPosition());
+                    area.setCaretPosition(caretPos);
+                } catch (BadLocationException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+            private static int getTabCount(String t) {
+                var c = 0;
+                for (var i = 0; i < t.length(); i++) {
+                    if (t.charAt(i) == '\t')
+                        c++;
+                    else
+                        return c;
+                }
+
+                return c;
+            }
+
+            private static int getOpenBraceCount(RSyntaxDocument doc) {
+                int openCount = 0;
+                for (Token t : doc) {
+                    if (t.getType() == Token.SEPARATOR && t.length() == 1) {
+                        char ch = t.charAt(0);
+                        if (ch == '{')
+                            openCount++;
+                        else if (ch == '}')
+                            openCount--;
+                    }
+                }
+
+                return openCount;
+            }
+        };
     }
 }

b7b7fa8b67226f515620945a260b54bb2985c810
tth05 <36999320+tth05@users.noreply.github.com>
2022-04-29 03:44:04 +0200

    Fix crash on completion in scripts because empty package is missing in element info

--- src/main/java/com/github/minecraft_ta/totalDebugCompanion/jdt/impls/DummyJarPackageFragmentRoot.java
+++ src/main/java/com/github/minecraft_ta/totalDebugCompanion/jdt/impls/DummyJarPackageFragmentRoot.java
@@ -4,13 +4,27 @@ import com.github.minecraft_ta.totalDebugCompanion.jdt.JDTHacks;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.compiler.CharOperation;
 import org.eclipse.jdt.internal.core.JarPackageFragmentRoot;
 import org.eclipse.jdt.internal.core.OpenableElementInfo;
+import org.eclipse.jdt.internal.core.util.HashtableOfArrayToObject;
+
+import java.util.ArrayList;
 
 public class DummyJarPackageFragmentRoot extends JarPackageFragmentRoot {
 
     public DummyJarPackageFragmentRoot() {
         super(new RootResourceImpl(), new Path("dummy-jar-path"), JDTHacks.DUMMY_JAVA_PROJECT, null);
+    }
+
+    @Override
+    protected Object createElementInfo() {
+        var info = super.createElementInfo();
+        var table = new HashtableOfArrayToObject();
+        // Empty package
+        table.put(CharOperation.NO_STRINGS, new ArrayList[]{EMPTY_LIST, EMPTY_LIST});
+        JDTHacks.setField(info, "rawPackageInfo", table);
+        return info;
     }
 
     @Override
